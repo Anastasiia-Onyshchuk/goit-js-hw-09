c004d124d58ec77b6825fab903dc034f2eac77ca
Anastasiia-Onyshchuk <nastyaonyshchuk07@gmail.com>
2023-08-19 09:25:47 +0300

    color-switched

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timerID;
+
+refs.startBtn.addEventListener('click', () => {
+    refs.startBtn.classList.add('disabled')
+    refs.startBtn.disabled = true;
+    timerID = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+        
+    }, 1000);
+});
+refs.stopBtn.addEventListener('click', () => {
+    refs.startBtn.classList.remove('disabled');
+    refs.startBtn.disabled = false;
+    clearInterval(timerID);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,19 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// const form = document.querySelector('form');
+// form.addEventListener('submit', createPromise())
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//   })
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
