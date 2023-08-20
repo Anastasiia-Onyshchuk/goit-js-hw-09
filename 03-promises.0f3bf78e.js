dd41014b85702feed38633ad0a34dceb45f00fdf
Anastasiia-Onyshchuk <113352486+Anastasiia-Onyshchuk@users.noreply.github.com>
2023-08-20 20:56:28 +0000

    Deploying to gh-pages from @ Anastasiia-Onyshchuk/goit-js-hw-09@a776fcc8ef8820fb644b0e3ab1a738eb428905d9 🚀

--- 03-promises.0f3bf78e.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector("form"),delay:document.querySelector('input[name ="delay"]'),step:document.querySelector('input[name = "step"]'),amount:document.querySelector('input[name = "amount"]')}).form.addEventListener("submit",new Promise(((e,t)=>{Math.random()})));
-//# sourceMappingURL=03-promises.0f3bf78e.js.map
--- 03-promises.c03d8e2d.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector("form"),document.querySelector('input[name ="delay"]'),document.querySelector('input[name = "step"]'),document.querySelector('input[name = "amount"]')][0].addEventListener("submit",new Promise((function(e,t){Math.random()})));
-//# sourceMappingURL=03-promises.c03d8e2d.js.map
